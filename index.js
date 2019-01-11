38694633c94e20e0dee53775eaa403cab7b1b616
David Stewart <davidstewart@gmail.com>
2019-01-11 01:57:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+function getFirstSelector(selector) {
+   return document.querySelector(selector)
+}
+var test = []
+
+function nestedTarget() {
+ 
+   return document.querySelector('div.target')
+   
+}
+
+function increaseRankBy(n) {
+  
+}
