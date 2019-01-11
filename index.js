4b10c6f3a05fee9c4e26be58ebd32abbc3ba991e
David Stewart <davidstewart@gmail.com>
2019-01-11 04:48:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
 
+function getFirstSelector(selector) {
+   return document.querySelector(selector)
+}
+var test = []
+
+function nestedTarget() {
+ 
+   return document.querySelector('div.target')
+   
+}
+
+function increaseRankBy(n) {
+var mainL = document.getElementsByClassName('ranked-list').length
+for (var i = 0; i < mainL; i++) {
+  var li = document.getElementsByClassName('ranked-list')[i]
+  for (var d = 0; d < li.children.length; d++) {
+    var update = li.children[d]
+    update.textContent = parseInt(update.textContent,10) + n
+  }
+}
+}
+
+function deepestChild() {
+  var node = document.getElementById('grand-node')
+  var i = 0
+  var final = node.children[0] 
+  
+  while (final.children.length === 1) {
+    final = final.children[0]
+    
+  }
+  return final
+ 
+  
+  
+  
+  
+  
+ 
+
+
+ //while (node.chilren.length > 0) {
+   //var node2 = node.children[i++]
+ 
+ 
+  
+  
+
+  
+}
+
+//<div id="grand-node">
+  //      <div>
+   //       <div>
+   //         <div>
+  //            <div>
+   //             boo!
+   //           </div>
+   //         </div>
+   //       </div>
+    //    </div>
+   //   </div>
+
+
+
