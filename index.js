2f09a52c2f060c907ec82bf4302d748677d81fc8
David Stewart <davidstewart@gmail.com>
2019-01-11 02:37:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var li = document.getElementsByClassName('ranked-list')[0]
 console.log(li.children[0])
 var update = li.children[0]
 console.log(update)
-return update.parseInt()
+return 
 //update.textContent = parseInt(update)
 
 // for (var i = 0; i < mainL; i++) {
