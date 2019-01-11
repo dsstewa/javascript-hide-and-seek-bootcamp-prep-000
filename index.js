d526d438d1e160120dfa37a2b5ec8a641be29d6a
David Stewart <davidstewart@gmail.com>
2019-01-11 02:19:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function increaseRankBy(n) {
  mainL = document.getElementsByClassName('ranked-list').length
  for (var i = 0; i < mainL; i++) {
    var li = document.getElementsByClassName('ranked-list')[i]
-   return li.children
+   return li.children[0]
    
  }
  
