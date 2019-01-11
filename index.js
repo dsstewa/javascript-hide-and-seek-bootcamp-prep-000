74d96fb9561fd0a417142d967c8869e61ce8b105
David Stewart <davidstewart@gmail.com>
2019-01-11 02:03:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,22 @@ function increaseRankBy(n) {
   
   
 }
+
+// body>
+//    <main id="app">
+//      <ul class="unranked-list">
+//      <li>2</li>
+//      <li>5</li>
+//        <li>4</li>
+//      </ul>
+
+//      <ul class="ranked-list">
+//      <li>1</li>
+        <li>2</li>
+      </ul>
+
+      <ul class="ranked-list">
+        <li>12</li>
+        <li>11</li>
+        <li>10</li>
+      </ul>
